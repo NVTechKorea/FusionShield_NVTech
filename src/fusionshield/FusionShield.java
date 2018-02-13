ae4ca645dd7a875788ee608e1d1d5cc0ad0db90b
NVTechKorea <32839744+NVTechKorea@users.noreply.github.com>
2018-02-13 14:13:53 +0900

    Stablized..?

--- src/fusionshield/FusionShield.java
+++ src/fusionshield/FusionShield.java
@@ -1,10 +1,11 @@
-//WITH MAIN
 package fusionshield;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -23,62 +24,126 @@ import javax.swing.border.EmptyBorder;
 //Version: 5.0 Prototype 2 NVTech Edition
 //Build: NVT500001P2
 
-public class FusionShield extends JFrame {
-
+public class FusionShield extends JPanel {
+	private int tried = 0;
 	private JPanel contentPane;
-	private JFrame frame;
+	private static JFrame frame;
 	private JTextField pubKey;
 	private JTextField priKey;
 	private Scanner input = new Scanner(System.in);
 	public String version = "NVTech Edition 5.0 Prototype 2";
+	public String build = "NVT500001P2";
 	private String baseversion = "DRX404001SR";
 	public String OS;
 	String superpath;
+	static JFrame frameB = new JFrame("Fusion Shield 5.0 NVTech Edition");
 	public String home;
 	public static String path_userdat;
 	public static String path_privateData;
 	public static String path_des;
+	public static String path_pubkey;
+	public static String path_prikey;
 	public String usrid = null;
 	public String usrpw = null;
 	private String cached1;
+	public static boolean pass = false;
 	private String cached2;
-	public boolean pass = false;
 	public boolean exist = false;
 	private JButton loginButton = new JButton("Login");
 	JButton newUserButton = new JButton("Create User");
 
 	/**
 	 * Launch the application.
+	 * 
+	 * @return
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FusionShield frame = new FusionShield();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public FusionShield() {
+
+	public static void main(String[] args) {
+		print("===STARTUP===");
+		print("STARTUP AUTO CONFIGURATOR 0.0.1");
+		print("POWERED BY: DYNAMICNEO");
+		print("Other informations: null");
+		print("=============");
+		defaultstart();
+	}
+
+	public static void frameshow() {
+		if (pass) {
+			frameB = new JFrame();
+			frameB.setVisible(false);
+			frameB.setResizable(false);
+			frame = new JFrame();
+			frame.setVisible(false);
+			frame.setResizable(false);
+		} else {
+			try {
+				frameB.setBounds(100, 100, 450, 300);
+				frameB.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+				FusionShield fs = new FusionShield();
+				fs.startAsVisual();
+				frameB.add(fs);
+				frameB.setResizable(false);
+				frameB.setVisible(true);
+			} catch (Exception e) {
+				print("Default system failure.");
+				print("Starting up from Safemode...");
+				///////////////////////////////
+				//Make an OBJECT of safe mode//
+				///////////////////////////////
+				try {
+					//Safe mode Startup method
+
+				} catch (Exception e1) {
+					e1.printStackTrace();
+					print("/////////////////////////////");
+					print("/////////////////////////////");
+					print("/////////////////////////////");
+					print("FAIL WHILE ACCESSING THE DISK");
+					print("/////////////////////////////");
+					print("/////////////////////////////");
+					print("/////////////////////////////");
+				}
+			}
+		}
+	}
+
+	public static void defaultstart() {
+		print("Copyright (C) NV Technology 2017 - 2018. All rights reserved.");
+		print("Do not distribute!!!");
+		print("Software written in Oracle Java JRE 1.8.0.");
+		print("NVTechnology Test OS 0.0.2.");
+		print("Starting up: DEFAULT");
+		frameshow();
+	}
+
+	public static void print(String a) {
+		System.out.println(a);
+	}
+
+	public void chkdes() throws Exception {
+		File checker = new File(path_des);
+		if (checker.exists()) {
+			FileReader des;
+			des = new FileReader(path_des);
+			String cached3 = null;
+			BufferedReader br = new BufferedReader(des);
+			cached3 = br.readLine();
+			print("DES detected. Unable to continue.\nData recorded: " + cached3);
+			JOptionPane.showMessageDialog(frameB, "DES detected. Unable to continue.\nData recorded: " + cached3);
+			System.exit(0);
+		}
+	}
+
+	public void startAsVisual() throws Exception {
 		varInit();
+		chkdes();
 
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		contentPane = this;
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		pubKey = new JTextField();
@@ -104,8 +169,54 @@ public class FusionShield extends JFrame {
 
 		newUserButton.setBounds(125, 243, 117, 29);
 		contentPane.add(newUserButton);
+		boolean auto = autoLoginAgent0();
+		if (auto) {
+			System.out.println(auto);
+			autoLoginAgent1();
+		} else {
+			GUI_ActionListen();
+		}
+	}
 
-		GUI_ActionListen();
+	public boolean autoLoginAgent0() {
+		boolean auto = false;
+		try {
+			File pubkey;
+			pubkey = new File(path_pubkey);
+			if (pubkey.exists()) {
+				File prikey;
+				prikey = new File(path_prikey);
+				if (prikey.exists()) {
+					print("AutoFillAgent: AutoFill data found.");
+					auto = true;
+				}
+			}
+			if (auto == false) {
+				print("AutoFillAgent: AutoFill data not found.");
+			}
+		} catch (Exception e) {
+			System.out.println("Error while reading file.");
+		}
+		return auto;
+	}
+
+	public void autoLoginAgent1() {
+		String cached3 = null;
+		try {
+			FileReader userdat;
+			FileReader userdat2;
+			userdat = new FileReader(path_pubkey);
+			userdat2 = new FileReader(path_prikey);
+			BufferedReader br = new BufferedReader(userdat);
+			BufferedReader br2 = new BufferedReader(userdat2);
+			cached1 = br.readLine();
+			cached2 = br2.readLine();
+			cached3 = cached1 + " / " + cached2;
+			String cached4 = encryptionEngine(cached3);
+			loginUI(cached4);
+		} catch (Exception e) {
+			System.out.println("Error while reading file.");
+		}
 	}
 
 	public void GUI_ActionListen() {
@@ -116,19 +227,20 @@ public class FusionShield extends JFrame {
 				cached1 = pubKey.getText();
 				cached2 = priKey.getText();
 				if (cached1.isEmpty()) {
-					JOptionPane.showMessageDialog(frame, "Public key is empty. Unable to continue.");
+					JOptionPane.showMessageDialog(frameB, "Public key is empty. Unable to continue.");
 				} else {
 					empty = false;
 				}
 				if (cached2.isEmpty()) {
-					JOptionPane.showMessageDialog(frame, "Private key is empty. Unable to continue.");
+					JOptionPane.showMessageDialog(frameB, "Private key is empty. Unable to continue.");
 				} else {
 					empty1 = false;
 				}
 				if (empty == false && empty1 == false) {
-					String cached3 = cached1 + version + cached2;
+					String cached3 = cached1 + " / " + cached2;
 					String cached4 = encryptionEngine(cached3);
 					loginUI(cached4);
+
 				}
 
 			}
@@ -140,12 +252,12 @@ public class FusionShield extends JFrame {
 				cached1 = pubKey.getText();
 				cached2 = priKey.getText();
 				if (cached1.isEmpty()) {
-					JOptionPane.showMessageDialog(frame, "Public key is empty. Unable to continue.");
+					JOptionPane.showMessageDialog(frameB, "Public key is empty. Unable to continue.");
 				} else {
 					empty = false;
 				}
 				if (cached2.isEmpty()) {
-					JOptionPane.showMessageDialog(frame, "Private key is empty. Unable to continue.");
+					JOptionPane.showMessageDialog(frameB, "Private key is empty. Unable to continue.");
 				} else {
 					empty1 = false;
 				}
@@ -154,6 +266,7 @@ public class FusionShield extends JFrame {
 				}
 			}
 		});
+
 	}
 
 	public void OSdetection() {
@@ -163,18 +276,24 @@ public class FusionShield extends JFrame {
 			path_userdat = "C://FusionShield//userdat.txt";
 			path_privateData = "C://FusionShield//privateData";
 			path_des = "C://FusionShield//des.txt";
+			path_pubkey = "C://FusionShield//alg1_pub.txt";
+			path_prikey = "C://FusionShield//alg1_pri.txt";
 		} else if (OS.startsWith("Mac")) {
 			home = System.getProperty("user.home");
 			superpath = home + "/FusionShield";
 			path_userdat = home + "/FusionShield/userdat.txt";
 			path_privateData = home + "/FusionShield/privateData";
 			path_des = home + "/FusionShield/des.txt";
+			path_pubkey = home + "/FusionShield/alg1_pub.txt";
+			path_prikey = home + "/FusionShield/alg1_pri.txt";
 		} else if (OS.contains("linux")) {
 			home = System.getProperty("user.home");
 			superpath = home + "/FusionShield";
 			path_userdat = home + "/FusionShield/userdat.txt";
 			path_privateData = home + "/FusionShield/privateData";
 			path_des = home + "/FusionShield/des.txt";
+			path_pubkey = home + "/FusionShield/alg1_pub.txt";
+			path_prikey = home + "/FusionShield/alg1_pri.txt";
 		}
 	}
 
@@ -190,10 +309,10 @@ public class FusionShield extends JFrame {
 		System.out.println("Checking encrypted data...");
 		checkUserdataExistance();
 		if (exist == true) {
-			System.out.println("Encrypted data exist. Starting login UI...");
+			System.out.println("Starting login UI...");
 			newUserButton.setEnabled(false);
 		} else {
-			System.out.println("Encrypted data does not exist. Creating...");
+			System.out.println("Creating...");
 			loginButton.setEnabled(false);
 		}
 	}
@@ -261,6 +380,7 @@ public class FusionShield extends JFrame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		JOptionPane.showMessageDialog(frameB, "Encrypted data created. Please restart.");
 		System.out.println("Encrypted data created. Please restart.");
 		System.exit(0);
 	}
@@ -305,25 +425,37 @@ public class FusionShield extends JFrame {
 		}
 	}
 
-	public boolean loginUI(String encryptedinput) {
-		FileReader userdat;
+	public void loginUI(String encryptedinput) {
 		try {
+			FileReader userdat;
 			userdat = new FileReader(path_userdat);
 			String cached3 = null;
 			BufferedReader br = new BufferedReader(userdat);
 			cached3 = br.readLine();
 			if (cached3.equals(encryptedinput)) {
-				JOptionPane.showMessageDialog(frame, "Login Successful.");
+				System.out.println("Login Successful.");
+				System.out.println("PASS!");
+				JOptionPane.showMessageDialog(frameB, "Login Successful.");
 				pass = true;
+				print("Your pass var is now: " + pass);
+				frameshow();
+				//Your main program here
+				
+				
 			} else {
-				JOptionPane.showMessageDialog(frame, "Login Failure.");
+				JOptionPane.showMessageDialog(frameB, "Login Failure.");
+				System.out.println("Login Failure.");
+				System.out.println("FAIL!");
+				tried++;
+				if (tried > 5) {
+					locker();
+				}
 				pass = false;
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return pass;
 	}
 
 	public String encryptionEngine(String key) {
@@ -345,11 +477,6 @@ public class FusionShield extends JFrame {
 			throw new RuntimeException(ex);
 		}
 		return res;
-	}
-
-	public boolean launch() {
-
-		return pass;
 	}
 
 	public static void locker() throws IOException {
@@ -386,7 +513,4 @@ public class FusionShield extends JFrame {
 		System.exit(0);
 	}
 
-	public static void print(String a) {
-		System.out.println(a);
-	}
 }
